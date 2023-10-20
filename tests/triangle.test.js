90fdbb5f1f71bf1234d180ebbc997a148ffa1e17
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-20 17:01:09 -0500

    Change triangle size so text fits

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -5,13 +5,13 @@ describe("Triangle", () => {
     it("should return a triangle element", () => {
       const svg = new Triangle();
       expect(svg.render()).toEqual(
-        `<polygon points="0,0 150,200 300,0" fill="white" />`
+        `<polygon points="0,35 150,200 300,35" fill="white" />`
       );
     });
     it("should return a blue triangle element", () => {
       const svg = new Triangle("blue");
       expect(svg.render()).toEqual(
-        `<polygon points="0,0 150,200 300,0" fill="blue" />`
+        `<polygon points="0,35 150,200 300,35" fill="blue" />`
       );
     });
   });
