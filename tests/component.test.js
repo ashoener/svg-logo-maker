02b4aca15778c241af1deb99d2c72eeabd4c68ac
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-20 16:26:56 -0500

    Fixed typo in component test

--- tests/component.test.js
+++ tests/component.test.js
@@ -2,7 +2,7 @@ const { default: Component } = require("../lib/component");
 
 describe("Component", () => {
   describe("render", () => {
-    it("should throw an error an error", () => {
+    it("should throw an error", () => {
       const component = new Component();
       expect(() => component.render()).toThrow();
     });
