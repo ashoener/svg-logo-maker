2e4f0d4805bbba861d726f3b8fd1cc5d5f0ef465
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-20 16:26:36 -0500

    Add circle component

---
+++ tests/circle.test.js
@@ -0,0 +1,18 @@
+const { default: Circle } = require("../lib/circle");
+
+describe("Circle", () => {
+  describe("render", () => {
+    it("should return a circle element", () => {
+      const svg = new Circle();
+      expect(svg.render()).toEqual(
+        `<circle cx="150" cy="100" r="80" fill="white" />`
+      );
+    });
+    it("should return a blue element", () => {
+      const svg = new Circle("blue");
+      expect(svg.render()).toEqual(
+        `<circle cx="150" cy="100" r="80" fill="blue" />`
+      );
+    });
+  });
+});
