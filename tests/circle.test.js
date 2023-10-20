16e68a1f8f36d8315367f98cfe35cc089d204c3b
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-20 17:03:34 -0500

    Change circle radius to match square size

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -5,13 +5,13 @@ describe("Circle", () => {
     it("should return a circle element", () => {
       const svg = new Circle();
       expect(svg.render()).toEqual(
-        `<circle cx="150" cy="100" r="80" fill="white" />`
+        `<circle cx="150" cy="100" r="75" fill="white" />`
       );
     });
     it("should return a blue circle element", () => {
       const svg = new Circle("blue");
       expect(svg.render()).toEqual(
-        `<circle cx="150" cy="100" r="80" fill="blue" />`
+        `<circle cx="150" cy="100" r="75" fill="blue" />`
       );
     });
   });
